44c2e2b2f4e4887d7eb7eae9abfdc7928f78c330
ldss2002 <32168991+ldss2002@users.noreply.github.com>
2017-11-30 17:21:04 +0000

    Add files via upload

---
+++ retangulo.java
@@ -0,0 +1,21 @@
+package Pacote1;
+
+import java.io.*;
+
+public class Retângulo {
+
+	public static void main(String[] args) throws IOException {
+		System.out.println("Qual o Comprimento?");
+			String C = "";
+			InputStreamReader LerS = new InputStreamReader (System.in);
+			BufferedReader GuardaS = new BufferedReader (LerS);
+			C = GuardaS.readLine();
+		System.out.println("Qual a Largura?");
+			String L = "";
+			InputStreamReader LerSL = new InputStreamReader (System.in);
+			BufferedReader GuardaSL = new BufferedReader (LerSL);
+			L = GuardaSL.readLine();
+		System.out.printf("A Área é %d.", Integer.valueOf(C)*Integer.valueOf(L));
+
+	}
+}
