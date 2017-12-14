ef6c1d159902b66c584a1456d8a892896a00e6bb
ldss2002 <32168991+ldss2002@users.noreply.github.com>
2017-12-14 15:32:24 +0000

    Add files via upload

---
+++ fatorial1.java
@@ -0,0 +1,18 @@
+package fatorial;
+import java.util.Scanner;
+public class fatorial1 {
+
+   
+    public static void main(String[] args)throws Exception 
+    {int n,fat=1;
+    Scanner LerS= new Scanner(System.in);
+    System.out.print("Digite um inteiro positivo inferior a 32:");
+    n=LerS.nextInt();
+    for(int M=n;M>=1;M--)
+        fat*=M;
+    System.out.printf("%d!=%d",n, fat);
+        
+        // TODO code application logic here
+    }
+    
+}
