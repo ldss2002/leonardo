db7ca1a1323dbb5841848f45f007800a073f6eec
ldss2002 <32168991+ldss2002@users.noreply.github.com>
2018-01-11 16:24:11 +0000

    Update Calculadora.java

--- Calculadora.java
+++ Calculadora.java
@@ -1,9 +1,29 @@
-package pacote1;
+import java.util.Scanner;
 
-public class Calculadora {
+public class calculadora{
 
-	public static void main(String[] args) {		
-
+	public static void main(String[] args) {
+		Scanner dados = new Scanner (System.in);
+		double numero_1, numero_2, total = 0;
+		String operador;
+		System.out.println("Qual o Operando 1?");
+		numero_1 = dados.nextDouble();
+		System.out.println("Qual o Operando 2?");
+		numero_2 = dados.nextDouble();
+		System.out.println("Qual o Operador?");
+		operador = dados.next();		
+		switch (operador) {
+		case "-" :  total = numero_1 - numero_2;
+		break;
+		case "+" : total = numero_1 + numero_2;
+		break;
+		case "/" : total = numero_1 / numero_2;
+		break;
+		case "*" : total = numero_1 * numero_2;
+		break;
+		}
+		System.out.println("Total = " + total);
+		
 	}
 
 }
