34c1897708c766e8994ffa430458fcb77edf3aca
ldss2002 <32168991+ldss2002@users.noreply.github.com>
2018-01-26 15:23:50 +0000

    Update maiorde3.java

--- maiorde3.java
+++ maiorde3.java
@@ -1,31 +1,22 @@
 package maiorde3;
-
 import java.util.Scanner;
-
 public class maiorde3 {
-
-	public static void main(String[] args) {
-		Scanner dados = new Scanner (System.in);
-		double n1, n2, n3, maior;
-		//Variaveis
-		System.out.println("Qual o primeiro número?");
-		n1 = dados.nextDouble();
-		System.out.println("Qual o segundo número?");
-		n2 = dados.nextDouble();
-		System.out.println("Qual o terceiro número?");
-		n3 = dados.nextDouble();
-		
-		if (n1>n2)
-			maior = n1;
-		else if (n2>n3)
-			maior = n2;
-		else 
-			maior = n3;
-		System.out.print("Maior = " + maior);
-		
-				
-		
-
+	static int maiorde3 (int X, int Y, int Z) 
+	{if (X<Y)
+		X=Y;
+	if (X<Z)
+		X=Z;
+	return X;}
+	public static void main (String [] args) throws Exception{
+		int x, y, z;
+		Scanner LerS = new Scanner (System.in);
+		System.out.print("Digite o primeiro valor (x)");
+		x = LerS.nextInt();
+		System.out.print("Digite o segundo valor (y)");
+		y = LerS.nextInt();
+		System.out.print("Digite o terceiro valor (z)");
+		z = LerS.nextInt();
+		System.out.println("Maior = "+ maiorde3 (x, y, z));	
+	}
 	}
 
-}
